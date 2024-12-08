552845ea646e88758de90c148ae36b5186b1f95f
wwedwa <w.wallace.edwards@gmail.com>
2024-12-08 11:10:38 -0500

    Adds time analysis. This will be included in all following days

--- day_7.cc
+++ day_7.cc
@@ -110,10 +110,29 @@ ulong PartTwo(Infoformat& info) {
 }
 
 int main() {
+  auto start = std::chrono::steady_clock::now();
   Infoformat info = GetInfo();
+  auto read_done = std::chrono::steady_clock::now();
+
   ulong answer_one = PartOne(info);
+  auto part_one_done = std::chrono::steady_clock::now();
+
   ulong answer_two = PartTwo(info);
-  std::cout << "Part One: " << answer_one << std::endl;
-  std::cout << "Part Two: " << answer_two << std::endl;
+  auto part_two_done = std::chrono::steady_clock::now();
+  std::cout << "Part One Answer: " << answer_one << std::endl;
+  std::cout << "Part Two Answer: " << answer_two << std::endl;
+
+  // Calculate run time of read, part one, and part two
+  double read_time = std::chrono::duration_cast<
+    std::chrono::duration<double>>(read_done - start).count();
+  double part_one_time = std::chrono::duration_cast<
+    std::chrono::duration<double>>(part_one_done - read_done).count();
+  double part_two_time = std::chrono::duration_cast<
+    std::chrono::duration<double>>(part_two_done - part_one_done).count();
+  std::cout << "\n------------Time analysis------------\n"
+            << "READ TIME: " << read_time << " seconds\n"
+            << "PART ONE TIME: " << part_one_time << " seconds\n"
+            << "PART TWO TIME: " << part_two_time << " seconds" << std::endl;
+            
   return 0;
 }
