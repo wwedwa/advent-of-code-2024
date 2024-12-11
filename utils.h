26ddeb0eafda100b494ae95bf9a0cac7e8bc694d
wwedwa <w.wallace.edwards@gmail.com>
2024-12-10 22:31:44 -0500

    Adds generic implementations of BFS and DFS

--- utils.h
+++ utils.h
@@ -14,6 +14,7 @@
 #include <vector>
 #include <queue>
 #include <deque>
+#include <stack>
 #include <list>
 #include <utility>
 #include <tuple>
@@ -21,6 +22,7 @@
 #include <climits>
 #include <numeric>
 #include <regex>
+#include <functional>
 
 namespace aoc {
 
@@ -60,6 +62,140 @@ std::vector<std::string> FindAllRegex(std::string input, std::string pattern);
 // Returns true if the input and pattern are exact matches. Otherwise, false
 bool ExactRegexMatch(std::string input, std::string pattern);
 
+/**
+ * @brief A generic BFS algorithm that allows optional processing of the current
+ * path
+ * 
+ * Nodes must be uniquely identifiable so that DFS can distinguish between
+ * nodes that have been visited and nodes that have not. Also nodes must have
+ * an ordering so that they can be placed in maps and sets
+ * 
+ * @param start_node The node to start the search at
+ * @param GetNeighbors A function that returns the neighbors of the current node
+ * @param ProcessPath An optional function that takes the current path of nodes 
+ * for any neccessary processing. Called every time BFS moves to a new node.
+ * @param EndCondition An optional function that is called every time BFS moves 
+ * to a new node. If true, the search ends. Default is to always return false.
+ * It is called after ProcessNode
+ */
+template <typename NodeType>
+void BFS(
+  const NodeType& start_node,
+  const std::function<std::vector<NodeType>(const NodeType&)>& GetNeighbors,
+  const std::function<void(const std::vector<NodeType>&)>& ProcessPath = [](const std::vector<NodeType>&) {},
+  const std::function<bool(const std::vector<NodeType>&)>& EndCondition = [](const std::vector<NodeType>&) { return false; }
+) {
+  std::queue<NodeType> node_queue;  // Current queue of nodes to visit
+  std::set<NodeType> visited;  // Nodes that have already been visited
+  std::vector<NodeType> path;  // Current path that BFS is on
+  std::map<NodeType, int> node_depth;  // Track depth of all nodes for constructing path
+  node_queue.push(start_node);
+  visited.insert(start_node);
+  path.push_back(start_node);
+  node_depth[start_node] = 0;
+
+  int curr_depth = 0;
+  while(!node_queue.empty()) {
+    // Get and remove next node from queue
+    NodeType curr_node = node_queue.front();
+    node_queue.pop();
+    // Update current path
+    if (curr_depth == node_depth[curr_node]) {
+      // If curr_node is on same level as last node in path, replace last node
+      path.pop_back();
+    }
+    path.push_back(curr_node);
+    curr_depth = node_depth[curr_node];  // Update current depth
+
+    // Now process the updated path
+    ProcessPath(path);
+    // Check if BFS should end
+    if (EndCondition(path)) {
+      return;
+    }
+    
+    for (const NodeType& neighbor : GetNeighbors(curr_node)) {
+      if (!visited.count(neighbor)) {
+        node_queue.push(neighbor);
+        visited.insert(neighbor);
+        node_depth[neighbor] = node_depth[curr_node] + 1;
+      }
+    }
+  }
+  return;
+}
+
+/**
+ * @brief A generic DFS algorithm that allows optional processing of the current
+ * path
+ * 
+ * Nodes must be uniquely identifiable so that DFS can distinguish between
+ * nodes that have been visited and nodes that have not. Also nodes must have
+ * an ordering so that they can be placed in maps and sets
+ * 
+ * @param start_node The node to start the search at
+ * @param GetNeighbors A function that returns the neighbors of the current node
+ * @param ProcessPath An optional function that takes the current path of nodes 
+ * for any neccessary processing. Called every time BFS moves to a new node.
+ * @param EndCondition An optional function that is called every time BFS moves 
+ * to a new node. If true, the search ends. Default is to always return false.
+ * It is called after ProcessNode
+ * @param revisit_nodes A flag for if the DFS algorithm should ever revisit nodes.
+ * Note that the algorithm will NEVER revisit nodes that are on the current path
+ * to avoid cycles. However, when in a different path, it will visit already
+ * visited nodes only if revisit_nodes is true. Defaults to false. This can be
+ * used to walk all non-cyclic paths starting from start_node
+ */
+template <typename NodeType>
+void DFS(
+  const NodeType& start_node,
+  const std::function<std::vector<NodeType>(const NodeType&)>& GetNeighbors,
+  const std::function<void(const std::vector<NodeType>&, int)>& ProcessPath = [](const std::vector<NodeType>&) {},
+  const std::function<bool(const std::vector<NodeType>&)>& EndCondition = [](const std::vector<NodeType>&) { return false; },
+  bool revisit_nodes = false
+) {
+  std::stack<NodeType> node_stack;  // Current stack of nodes to visit
+  std::set<NodeType> visited;  // Nodes that have already been visited
+  std::vector<NodeType> path;  // Current path that DFS is on
+  std::map<NodeType, int> node_depth;  // Track depth of all nodes for constructing path
+  node_stack.push(start_node);
+  visited.insert(start_node);
+  path.push_back(start_node);
+  node_depth[start_node] = 0;
+
+  int curr_depth = 0;
+
+  while (!node_stack.empty()) {
+    NodeType curr_node = node_stack.top();
+    node_stack.pop();
+
+    // Update current path
+    while (curr_depth >= node_depth[curr_node]) {
+      --curr_depth;
+      path.pop_back();
+    }
+    path.push_back(curr_node);
+    curr_depth = node_depth[curr_node];  // Update current depth
+
+    // Now process the updated path
+    ProcessPath(path, curr_depth);
+    // Check if DFS should end
+    if (EndCondition(path)) {
+      return;
+    }
+    // Only add neighbors if it hasn't been visited (if revisit_nodes is false)
+    // or if it has been visited in the current path
+    if (!visited.count(curr_node) || revisit_nodes) {
+      visited.insert(curr_node);
+      for (const NodeType& neighbor : GetNeighbors(curr_node)) {
+        if (std::find(path.begin(), path.end(), neighbor) == path.end()) {
+          node_depth[neighbor] = node_depth[curr_node] + 1;
+          node_stack.push(neighbor);
+        }
+      }
+    }
+  }
+}
 
 
 // Structure to represent a simple grid with barriers and a moveable player 
