2a370975c17919adfceab31126497aba7a4c142a
wwedwa <w.wallace.edwards@gmail.com>
2024-12-11 20:13:08 -0500

    Adds day 11 part 2 solution

--- day_11.cc
+++ day_11.cc
@@ -1,27 +1,28 @@
 #include "utils.h"
 
-using Infoformat = std::list<long>;
+using ullong = unsigned long long;
+using Infoformat = std::list<ullong>;
 
 Infoformat GetInfo() {
   Infoformat info;
   std::ifstream input("input.txt");
   std::string line;
   while (std::getline(input, line)) {
-    std::vector<long> num_vec = aoc::GetLongs(aoc::split(line, " "));
+    std::vector<ullong> num_vec = aoc::GetULLongs(aoc::split(line, " "));
     // Copy vector into list for the insertion speed
-    info = std::list<long>(num_vec.begin(), num_vec.end());
+    info = std::list<ullong>(num_vec.begin(), num_vec.end());
   }
   return info;
 }
 
-std::vector<long> Transform(long num) {
+std::vector<ullong> Transform(ullong num) {
   // For easy digit counting and splitting
   std::string num_str = std::to_string(num);
   if (num == 0) {
     return {1};
   } else if (num_str.size() % 2 == 0) {
-    return {std::stol(num_str.substr(0, num_str.size() / 2)),
-            std::stol(num_str.substr(num_str.size() / 2))};
+    return {std::stoull(num_str.substr(0, num_str.size() / 2)),
+            std::stoull(num_str.substr(num_str.size() / 2))};
   } else {
     return {num * 2024};
   }
@@ -29,7 +30,7 @@ std::vector<long> Transform(long num) {
 
 void Blink(Infoformat& info) {
   for (auto it = info.begin(); it != info.end(); ++it) {
-    std::vector<long> new_nums = Transform(*it);
+    std::vector<ullong> new_nums = Transform(*it);
     if (new_nums.size() == 1) {
       *it = new_nums[0];
     } else {
@@ -40,26 +41,65 @@ void Blink(Infoformat& info) {
   }
 }
 
-long DFSApproach(const Infoformat& info, int depth) {
-  // Store value, depth, and id
-  long answer = 0;
+ullong DFSApproach(const Infoformat& info, int depth) {
+  ullong answer = 0;
 
-  auto GetNeighbors = [](const long& node) -> std::vector<long> {
+  // Key is stone value. Value is a map where key is n and value
+  // is a pair containing resulting number of stones after n blinks and
+  // if this value is final
+  std::map<ullong, std::map<int, std::pair<ullong, bool>>> cache;
+
+  auto GetNeighbors = [](const ullong& node) -> std::vector<ullong> {
     // If we are at max depth, no more neighbors
     return Transform(node);
   };
 
-  auto ProcessPath = [&answer, &depth](const std::vector<long>& path) {
-    if (path.size() == depth + 1) {
-      ++answer;
+  auto PreProcessPath = [&answer, &depth, &cache](const std::vector<ullong>& path) {
+    if (cache[path.back()][0].second) {
+      return;
+    }
+    if (path.size() != depth + 1) {
+      return;
+    }
+    for (int i = 0; i < path.size(); ++i) {
+      if (!cache.count(path[i])) {
+        cache[path[i]][depth - i] = {0, false};
+      }
+      ++cache[path[i]][depth - i].first;
+    }
+    ++answer;
+  };
+
+  auto PostProcessPath = [&depth, &cache](const std::vector<ullong>& path) {
+    for (int i = 0; i < path.size(); ++i) {
+      if (cache.count(path[i])) {
+        cache[path[i]][depth - i].second = true;
+      }
     }
   };
 
-  for (const long& start : info) {
-    aoc::DFS<long>(start,
+  auto EndCondition = [&answer, &depth, &cache](const std::vector<ullong>& path) -> bool {
+    if (cache.count(path.back())) {
+      std::pair<ullong, bool> cache_entry = cache[path.back()][depth - path.size() + 1];
+      if (cache_entry.second) {
+        answer += cache_entry.first;
+        for (int i = 0; i < path.size() - 1; ++i) {
+          if (cache.count(path[i])) {
+            cache[path[i]][depth - i].first += cache_entry.first;
+          }
+        }
+        return true;
+      }
+    }
+    return false;
+  };
+
+  for (const ullong& start : info) {
+    aoc::DFS<ullong>(start,
                   GetNeighbors,
-                  ProcessPath,
-                  [](const std::vector<long>&) { return false; },
+                  PreProcessPath,
+                  PostProcessPath,
+                  EndCondition,
                   true,  // revisit nodes
                   true,  // allow cycles
                   depth);  // max depth of cycle
@@ -75,9 +115,8 @@ int PartOne(Infoformat info) {
 }
 
 // Create function for finding out what a 0 becomes after n steps
-int PartTwo(Infoformat info) {
-  int answer = 0;
-  return answer;
+ullong PartTwo(Infoformat info) {
+  return DFSApproach(info, 75);
 }
 
 int main() {
@@ -88,7 +127,7 @@ int main() {
   int answer_one = PartOne(info);
   auto part_one_done = std::chrono::steady_clock::now();
 
-  int answer_two = PartTwo(info);
+  ullong answer_two = PartTwo(info);
   auto part_two_done = std::chrono::steady_clock::now();
 
   std::cout << "Part One Answer: " << answer_one << std::endl;
